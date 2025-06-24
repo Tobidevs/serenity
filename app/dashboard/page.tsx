0a560c1dc737cace35d5315c9ec21bb28a64460f
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-06-24 13:49:41 -0500

    Refactor userProfile

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { UserButton, UserProfile } from "@clerk/nextjs"
 export default function DashboardPage() {
   return (
     <div>
-        <UserProfile/>
+        
         <UserButton/>
       <div>Dashboard</div>
     </div>
