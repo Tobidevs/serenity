65ea8c089b6ebe1ad8edb976a4c9e5acb7933b46
Tobi Akere <tobiakere25@gmail.com>
2025-08-03 19:30:53 -0500

    Use bible store functionality

--- components/quiz.tsx
+++ components/quiz.tsx
@@ -1,5 +1,4 @@
-
-import { useBibleQuizStore } from "../store/useBibleQuizStore"
+import { useBibleQuizStore } from "../store/useBibleQuizStore";
 export const Quiz = () => {
   const { generateQuestion } = useBibleQuizStore();
 
@@ -7,10 +6,12 @@ export const Quiz = () => {
     <div>
       <h1>Quiz Component</h1>
       <p>This is a simple quiz component.</p>
-      
-      <button className="btn" onClick={() => generateQuestion()}>Start Quiz</button>
-    </div>
-  )
-}
 
-export default Quiz
+      <button className="btn" onClick={() => generateQuestion()}>
+        Start Quiz
+      </button>
+    </div>
+  );
+};
+
+export default Quiz;
