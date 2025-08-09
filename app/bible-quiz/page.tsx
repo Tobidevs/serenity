056c0a9b4a5d79243404a5f1c3c511d220b1295d
Tobi Akere <tobiakere25@gmail.com>
2025-08-09 14:36:57 -0500

    Created back button for quiz

--- app/bible-quiz/page.tsx
+++ app/bible-quiz/page.tsx
@@ -1,14 +1,25 @@
-"use client"
+"use client";
 import { Navbar } from "../../components/navbar";
 import { SearchBar } from "../../components/search-bar";
 import { Quiz } from "../../components/quiz";
+import { ChevronRightIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function BibleQuizPage() {
+  const router = useRouter();
   return (
-    <div className="w-full flex min-h-screen">
+    <div className="w-full flex flex-col min-h-screen">
       <Navbar />
       <SearchBar />
-       <div className="mt-15 pt-10 pl-5 pr-5 w-full flex flex-col items-center">
+      <div className="mt-15 pt-5 pl-5 pr-5 w-full flex flex-col items-center">
+        <div className="flex justify-between w-full">
+          <button
+            className="btn rounded-xl flex justify-center border-none shadow-none items-center p-3 bg-grey-main "
+            onClick={() => router.push("/bible-menu")}
+          >
+            <ChevronRightIcon className="rotate-180 text-grey-primary" />
+          </button>
+        </div>
         <Quiz />
       </div>
     </div>
