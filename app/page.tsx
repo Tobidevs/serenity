c1906fd73ce690e7f638d645e3d8843464ebe76e
Tobi Akere <tobiakere25@gmail.com>
2025-08-18 19:01:36 -0500

    Created help form section

--- app/page.tsx
+++ app/page.tsx
@@ -360,6 +360,23 @@ export default function Home() {
             </div>
           </Fade>
         </div>
+
+        <div className="flex flex-col w-full items-center justify-center mt-35 mb-5">
+          <Link href={"/help-serenity"}>
+            <FaHandsHelping
+              size={100}
+              className="text-lg md:text-2xl text-grey-primary"
+            />
+          </Link>
+
+          <h1 className="text-2xl text-grey-primary text-center font-bold mb-2">
+            Want to contribute? <br /> Click the Icon Above!
+          </h1>
+          <p className="text-grey-secondary text-center mb-4">
+            I would love your help! If you have ideas, suggestions, or want to
+            contribute, please fill out the form.
+          </p>
+        </div>
       </div>
       <Fade
         direction="up"
